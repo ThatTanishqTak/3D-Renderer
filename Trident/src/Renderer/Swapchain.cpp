15a4700312004a492ef68877f0c6ff724c2dec1a
ThatTanishqTak <tanishqtak3@gmail.com>
2025-06-29 00:34:12 +0100

    Fixed the white tint issue on the ImGui windows

--- Trident/src/Renderer/Swapchain.cpp
+++ Trident/src/Renderer/Swapchain.cpp
@@ -160,7 +160,7 @@ namespace Trident
     {
         for (const auto& a_Format : availableFormats)
         {
-            if (a_Format.format == VK_FORMAT_B8G8R8A8_SRGB && a_Format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+            if (a_Format.format == VK_FORMAT_B8G8R8A8_UNORM && a_Format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
             {
                 return a_Format;
             }
