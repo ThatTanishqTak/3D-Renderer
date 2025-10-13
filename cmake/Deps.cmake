553838c0659321447b4e46ad57f8be639d635039
ThatTanishqTak <tanishqtak3@gmail.com>
2025-10-13 09:47:05 +0100

    Fixing cmake errors

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -23,3 +23,37 @@ FetchContent_Declare(
   GIT_TAG docking
 )
 FetchContent_MakeAvailable(imgui)
+
+# Wrap Dear ImGui into a proper CMake target
+FetchContent_GetProperties(imgui)
+if (NOT imgui_POPULATED)
+  FetchContent_Populate(imgui)
+endif()
+set(IMGUI_DIR ${imgui_SOURCE_DIR})
+
+# Choose OpenGL backend for now
+find_package(OpenGL REQUIRED)
+
+add_library(imgui STATIC
+  ${IMGUI_DIR}/imgui.cpp
+  ${IMGUI_DIR}/imgui_draw.cpp
+  ${IMGUI_DIR}/imgui_tables.cpp
+  ${IMGUI_DIR}/imgui_widgets.cpp
+  ${IMGUI_DIR}/backends/imgui_impl_glfw.cpp
+  ${IMGUI_DIR}/backends/imgui_impl_opengl3.cpp
+  ${IMGUI_DIR}/backends/imgui_impl_vulkan.cpp
+)
+
+target_include_directories(imgui PUBLIC
+  ${IMGUI_DIR}
+  ${IMGUI_DIR}/backends
+)
+
+# Using GLAD as the GL loader; make sure that define is set
+target_compile_definitions(imgui PUBLIC IMGUI_IMPL_OPENGL_LOADER_GLAD)
+
+# Link deps
+target_link_libraries(imgui PUBLIC glfw glad OpenGL::GL)
+if (WIN32)
+  target_link_libraries(imgui PUBLIC Imm32)
+endif()
