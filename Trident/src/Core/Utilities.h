87466e836c7d1679cf68ac30d2110068f12e42c8
ThatTanishqTak <tanishqtak3@gmail.com>
2025-10-27 23:40:00 +0000

    Seperated Debug and Checked for PhysX

--- Trident/src/Core/Utilities.h
+++ Trident/src/Core/Utilities.h
@@ -194,7 +194,7 @@ void operator delete(void* ptr) noexcept;
 void* operator new[](std::size_t size);
 void operator delete[](void* ptr) noexcept;
 
-#ifdef _DEBUG
+#ifndef _DEBUG
 // Core log macros
 #define TR_CORE_TRACE(...) ::Trident::Utilities::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define TR_CORE_INFO(...) ::Trident::Utilities::Log::GetCoreLogger()->info(__VA_ARGS__)
